9f375e9bf8b917f4a5cd35ee71268a90cccf52f0
훈성 <phunseong@gmail.com>
2021-12-24 19:36:03 +0900

    Operator - flatMap

---
+++ Operator/Operator(flatMap).kt
@@ -0,0 +1,36 @@
+package com.hunseong.rxpginkotlin
+
+import io.reactivex.Observable
+
+fun main() {
+    val observable = Observable.just(1, 4, 7, 10)
+
+    // flatMap을 통해 각 아이템(1, 4, 7, 10)에 대한 새로운 observable return
+    // 1에 대한 새로운 observable은 1,2,3을 발행
+    // 4에 대한 새로운 observable은 4,5,6을 발행
+    // ...
+    observable
+        .flatMap { value ->
+            Observable.create<Int> {
+                it.onNext(value)
+                it.onNext(value+1)
+                it.onNext(value+2)
+            }
+        }
+        .subscribe ({
+            println("onNext - $it")
+        })
+}
+
+//onNext - 1
+//onNext - 2
+//onNext - 3
+//onNext - 4
+//onNext - 5
+//onNext - 6
+//onNext - 7
+//onNext - 8
+//onNext - 9
+//onNext - 10
+//onNext - 11
+//onNext - 12
